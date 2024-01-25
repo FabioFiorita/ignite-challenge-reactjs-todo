b6fdec247e136bb1a5b648ff54954d422fdb20be
Fabio Fiorita <fabiolfp@gmail.com>
2024-01-25 11:12:10 -0300

    feat: :sparkles: Add Button component with styles

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import styles from "./Button.module.css"
+import { PlusCircle } from "phosphor-react"
+
+export function Button() {
+    return (
+        <button className={styles.button}>
+            <span>Criar</span>
+            <PlusCircle size={16} />
+        </button>
+    )
+}
