dd529ca55e502d77c7cffc04e2dbcc13ddfc178e
Fabio Fiorita <fabiolfp@gmail.com>
2024-01-25 15:30:23 -0300

    feat: :sparkles: Update Input component styles and props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,16 @@
 import styles from "./Input.module.css"
 
-interface InputProps {
-    content: string;
-}
-
-export function Input({ content }: InputProps) {
+export function Input({
+    ...rest
+}: React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+>) {
     return (
         <input
             className={styles.input}
             type="text"
             placeholder="Adicione uma nova tarefa"
-            value={content} />
+            {...rest} />
     )
 }
