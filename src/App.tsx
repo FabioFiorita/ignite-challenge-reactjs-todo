af9ba218fa2e496a87b651e68033bbd5db369cdc
Fabio Fiorita <fabiolfp@gmail.com>
2024-01-25 15:32:40 -0300

    refactor: :recycle: Refactor App component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,73 @@
+import { v4 } from "uuid"
+
 import { Header } from "./components/Header";
-import { NewTask } from "./components/NewTask";
+import { Info } from "./components/Info";
+import { Empty } from "./components/Empty";
+import { Task } from "./components/Task";
 
 import styles from "./App.module.css"
 
 import "./global.css";
+import { useState } from "react";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+export interface TaskType {
+  id: string;
+  title: string;
+  completed: boolean;
+}
 
-function App() {
+export function App() {
+  const [tasks, setTasks] = useState<TaskType[]>([])
+  const [newTask, setNewTask] = useState("")
+  const noTasksCreated = tasks.length == 0;
+
+  function handleToggleSelected(taskToToggle: TaskType) {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskToToggle.id) {
+        task.completed = !task.completed
+        return task
+      }
+      return task
+    })
+
+    setTasks(updatedTasks)
+  }
+
+  function handleDelete(taskToDelete: TaskType) {
+    const tasksWithoutDeleted = tasks.filter(task => task.id != taskToDelete.id);
+    setTasks(tasksWithoutDeleted);
+  }
+
+  function handleAddNewTask() {
+    if (!newTask) {
+      return
+    }
+    const task: TaskType = {
+      id: v4(),
+      title: newTask,
+      completed: false
+    }
+
+    setTasks([...tasks, task])
+    setNewTask("")
+  }
+
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
-        <NewTask />
+        <div className={styles.newTask}>
+          <Input value={newTask} onChange={e => setNewTask(e.target.value)} />
+          <Button onClick={handleAddNewTask} />
+        </div>
+        <Info tasks={tasks} />
+        {noTasksCreated ? <Empty /> :
+          tasks.map(task => {
+            return <Task key={task.id} task={task} onToggleSelected={handleToggleSelected} onDelete={handleDelete} />
+          })
+        }
       </div>
     </>
   )
 }
-
-export default App
