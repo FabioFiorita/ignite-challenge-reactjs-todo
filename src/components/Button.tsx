6c907d7f2d6c3d16f91e136cd3200634417d874c
Fabio Fiorita <fabiolfp@gmail.com>
2024-01-25 15:31:59 -0300

    refactor: :recycle: Button component and import statement in main.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,12 @@
 import styles from "./Button.module.css"
 import { PlusCircle } from "phosphor-react"
 
-export function Button() {
+export function Button({
+    ...rest
+}: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) {
+
     return (
-        <button className={styles.button}>
+        <button className={styles.button} {...rest}>
             <span>Criar</span>
             <PlusCircle size={16} />
         </button>
